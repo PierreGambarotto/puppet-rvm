cc3447700d7b7f76ec00075a6e908d675570610b
Pierre Gambarotto <pierre.gambarotto@enseeiht.fr>
2011-06-07 10:16:55 +0200

    fact default_rvm : patch to handle new syntax

--- lib/facter/default_rvm.rb
+++ lib/facter/default_rvm.rb
@@ -1,10 +1,22 @@
 Facter.add("default_rvm_ruby_string") do
   setcode do
-    (%x{grep rvm_ruby_string= /usr/local/rvm/environments/default}||"").chomp.split('=')[1][1..-2]
+    s = %x{grep rvm_ruby_string= /usr/local/rvm/environments/default}
+    if s
+      s =~ /rvm_ruby_string='([^']*)'/
+      $1 
+    else
+      ""
+    end
   end
 end
 Facter.add("default_rvm_gem_home") do
   setcode do
-    (%x{grep GEM_HOME= /usr/local/rvm/environments/default}||"").chomp.split('=')[1][1..-2]
+    s = %x{grep GEM_HOME= /usr/local/rvm/environments/default}
+    if s
+      s =~ /GEM_HOME='([^']*)'/
+      $1 
+    else
+      ""
+    end
   end
 end
