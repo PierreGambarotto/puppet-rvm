e3880a5218a2612bd0e76c5f3fde1dfc2263c2c4
Pierre Gambarotto <pierre.gambarotto@n7.fr>
2011-10-28 17:12:14 +0200

    change url of install script and timeout

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class rvm {
     path => "/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin"
   }
   exec{"fetch rvm install script":
-    command => "curl -k -L http://rvm.beginrescueend.com/releases/rvm-install-head > /tmp/rvm_install",
+    command => "curl -k -L https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer > /tmp/rvm_install",
     unless => "grep -l '/usr/bin/env bash' /tmp/rvm_install",
     require => Package[curl]
   }
@@ -12,7 +12,7 @@ class rvm {
   exec{"rvm install":
     command => "chmod +x /tmp/rvm_install && /tmp/rvm_install",
     creates => "/usr/local/rvm/bin/rvm",
-    require => Exec["fetch rvm install script"]
+    require => [Exec["fetch rvm install script"], Package[git-core]]
   }
 
   group{rvm:
@@ -41,7 +41,7 @@ class rvm {
       path => "/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin",
       require => [$required, Exec["rvm install"]],
       command => "/usr/local/rvm/bin/rvm install $install_version",
-      timeout => "-1",
+      timeout => "0",
       logoutput => true,
       unless => "ls -l /usr/local/rvm/rubies/*$install_version*"
     }
@@ -61,7 +61,7 @@ class rvm {
 
     exec{"generate rvm conf for apache2":
       require => File["/usr/local/rvm/environments/default"],
-      command => "grep -E '(GEM_HOME|GEM_PATH|MY_RUBY_HOME|RUBY_VERSION)=' /usr/local/rvm/environments/default | sed -e 's/^\([^=]*\)=\(.*\)$/SetEnV \1 \2/' > /usr/local/rvm/environments/default_apache",
+      command => "grep -E '(GEM_HOME|GEM_PATH|MY_RUBY_HOME|RUBY_VERSION)=' /usr/local/rvm/environments/default | sed -e \"s/^\([^=]*\)=\([^;].*\);.*$/SetEnV \1 \2/\" > /usr/local/rvm/environments/default_apache",
      refreshonly => true
     }
     
